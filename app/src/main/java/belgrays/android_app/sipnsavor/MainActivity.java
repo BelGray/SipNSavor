92e1831f43f253a643401d544e81dd815b62a033
BelGray <goldendaniel186@gmail.com>
2023-08-23 16:35:39 +0300

    add colors of different themes & pictures

--- app/src/main/java/belgrays/android_app/sipnsavor/MainActivity.java
+++ app/src/main/java/belgrays/android_app/sipnsavor/MainActivity.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import belgrays.android_app.sipnsavor.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
